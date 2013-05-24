ae6e422fa47a1a8258dfccf572e56c9bef07452f
hslife <hslife@ac78c17a-de86-46ef-81f7-d3fdc1a3e730>
2013-05-24 18:42:53 +0000

    Criado o script de criação da base de dados. Feito ajustes na documentação do sistema. refs #610 @1h
    git-svn-id: svn://hslife.com.br/hslife/encontreaquipecas/trunk@9 ac78c17a-de86-46ef-81f7-d3fdc1a3e730

--- src/script-db.sql
+++ src/script-db.sql
@@ -44,5 +44,93 @@ CREATE TABLE `usuario` (
   UNIQUE KEY `login` (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-insert into usuario (ativo, dataCriacao, login, nome, senha, tipoUsuario, email) values (true, '2013-01-01', 'admin', 'Administrador do Sistema', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'ROLE_ADMIN', 'contato@hslife.com.br');
+create table endereco(
+	id bigint(20) not null auto_increment,
+	tipoLogradouro varchar(70) not null,
+	logradouro varchar(200) not null,
+	complemento varchar(100) not null, 
+	numero varchar(10) null,
+	bairro varchar(50) not null,
+	cidade varchar(100) not null, 
+	cep varchar(8) not null,
+	estado varchar(2) not null,
+	primary key(id)
+);
 
+create table telefone(
+	id bigint(20) not null auto_increment,
+	ddd varchar(5) not null,
+	numero varchar(10) not null,
+	tipoTelefone varchar(15) not null, 
+	primary key(id)
+);
+
+create table loja(
+	id bigint(20) not null auto_increment,
+	idUsuario bigint not null,
+	idEndereco bigint not null,	
+	nome varchar(200) not null,
+	contato varchar(200) not null,
+	cnpj varchar(14) not null, 
+	inscricaoEstadual varchar(10) null,
+	site varchar(100) null,
+	areaInteresse varchar(15) not null, 
+	primary key(id)
+);
+
+alter table loja add constraint fk_usuario_loja foreign key usuario (idUsuario) references usuario (id);
+alter table loja add constraint fk_endereco_loja foreign key endereco (idEndereco) references endereco (id);
+
+create table consumidor(
+	id bigint(20) not null auto_increment,
+	idUsuario bigint not null,
+	idEndereco bigint not null,	 
+	primary key(id)
+);
+
+alter table consumidor add constraint fk_usuario_consumidor foreign key usuario (idUsuario) references usuario (id);
+alter table consumidor add constraint fk_endereco_consumidor foreign key endereco (idEndereco) references endereco (id);
+
+create table loja_telefone(
+	idLoja bigint(20) not null,
+	idTelefone bigint(20) not null
+);
+
+alter table loja_telefone add constraint fk_loja_loja_telefone foreign key loja (idLoja) references loja (id);
+alter table loja_telefone add constraint fk_telefone_loja_telefone foreign key telefone (idTelefone) references telefone (id);
+
+create table consumidor_telefone(
+	idConsumidor bigint(20) not null,
+	idTelefone bigint(20) not null
+);
+
+alter table consumidor_telefone add constraint fk_consumidor_consumidor_telefone foreign key consumidor (idConsumidor) references consumidor (id);
+alter table consumidor_telefone add constraint fk_telefone_consumidor_telefone foreign key telefone (idTelefone) references telefone (id);
+
+create table banner(
+	id bigint(20) not null auto_increment,
+	idLoja bigint not null,
+	nomeArquivo varchar(255) not null,
+	caminho varchar(255) not null,
+	contentType varchar(50) not null, 
+	tamanho int not null,
+	primary key(id)
+);
+
+alter table banner add constraint fk_loja_banner foreign key loja (idLoja) references loja (id);
+
+create table produto(
+	id bigint(20) not null auto_increment,
+	idLoja bigint(20) not null,
+	nome varchar(255) not null,
+	fabricante varchar(255) not null,
+	modelo varchar(255) not null,
+	preco double not null,	
+	descricao text not null,
+	ano int not null, 
+	primary key(id)
+);
+
+alter table produto add constraint fk_loja_produto foreign key loja (idLoja) references loja (id);
+
+insert into usuario (ativo, dataCriacao, login, nome, senha, tipoUsuario, email) values (true, '2013-01-01', 'admin', 'Administrador do Sistema', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'ROLE_ADMIN', 'contato@hslife.com.br');
